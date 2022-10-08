3d50aa4210799aed5a270927d952780e1670760a
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-08 17:06:19 +0300

    login user request

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -11,7 +11,7 @@ export const registerUser = createAsyncThunk(
   `user/registerUser`,
   async (user, thunkAPI) => {
     try {
-      const resp = await customFetch.post(`/auth/testingRegister`, user);
+      const resp = await customFetch.post(`/auth/register`, user);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
@@ -21,7 +21,12 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   `user/loginUser`,
   async (user, thunkAPI) => {
-    console.log(`login user : ${user}`);
+    try {
+      const resp = await customFetch.post(`/auth/login`, user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
@@ -39,6 +44,19 @@ const userSlice = createSlice({
       toast.success(`Hello there ${user.name}`);
     },
     [registerUser.pending]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [loginUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      toast.success(`Welcome back ${user.name}`);
+    },
+    [loginUser.pending]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
