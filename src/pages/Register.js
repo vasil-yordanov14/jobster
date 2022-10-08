53f49c1d1c900b6e0564f24efcf39fe90638b9a3
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-08 17:24:54 +0300

    navigate to dashboard

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,6 +5,7 @@ import Wrapper from "../assets/wrappers/RegisterPage";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: ``,
@@ -17,7 +18,7 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
   const { user, isLoading } = useSelector((store) => store.user);
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -41,7 +42,13 @@ const Register = () => {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
-
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate(`/`);
+      }, 3000);
+    }
+  }, [user]);
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
