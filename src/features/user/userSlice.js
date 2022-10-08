6968fd8171a09156fc2143bf61381b05830fc062
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-08 17:17:12 +0300

    local storage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+  removeUserFromLocalStorage,
+} from "../../utils/localStorage";
 
 const initialState = {
   isLoading: false,
-  user: null,
+  user: getUserFromLocalStorage(),
 };
 
 export const registerUser = createAsyncThunk(
@@ -41,6 +46,7 @@ const userSlice = createSlice({
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
+      addUserToLocalStorage(user);
       toast.success(`Hello there ${user.name}`);
     },
     [registerUser.pending]: (state, { payload }) => {
@@ -54,6 +60,7 @@ const userSlice = createSlice({
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
+      addUserToLocalStorage(user);
       toast.success(`Welcome back ${user.name}`);
     },
     [loginUser.pending]: (state, { payload }) => {
