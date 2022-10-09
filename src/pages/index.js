c1de9407ae1552b143affae9c6bd42fc44614f3f
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-09 18:11:39 +0300

    protected route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Landing from "./Landing";
 import Error from "./Error";
-
+import ProtectedRoute from "./ProtectedRoute";
 import Register from "./Register";
 
-export { Landing, Error, Register };
+export { Landing, Error, Register, ProtectedRoute };
