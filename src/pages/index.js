80ec363265626ec0eab62b82da86bc9cb82334f4
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-09 13:35:29 +0300

    setup dashboard pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Landing from "./Landing";
 import Error from "./Error";
-import Dashboard from "./Dashboard";
+
 import Register from "./Register";
 
-export { Landing, Error, Dashboard, Register };
+export { Landing, Error, Register };
