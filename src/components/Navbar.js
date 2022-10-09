8cface3f55449092c43d4276a72ae6a99152efc2
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-09 15:32:30 +0300

    logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,10 @@ import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleSidebar } from "../features/user/userSlice";
+import { toggleSidebar, logoutUser } from "../features/user/userSlice";
 
 const Navbar = () => {
+  const [showLogout, setShowLogout] = useState(false);
   const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
@@ -29,17 +30,17 @@ const Navbar = () => {
           <button
             type="button"
             className="btn"
-            onClick={() => console.log(`toggle logout dropdown`)}
+            onClick={() => setShowLogout(!showLogout)}
           >
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogout ? `dropdown show-dropdown` : `dropdown`}>
             <button
               type="button"
               className="dropdown-btn"
-              onClick={() => console.log(`logout user`)}
+              onClick={() => dispatch(logoutUser())}
             >
               logout
             </button>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -43,6 +43,11 @@ const userSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      removeUserFromLocalStorage();
+    },
   },
   extraReducers: {
     [registerUser.pending]: (state) => {
@@ -76,5 +81,5 @@ const userSlice = createSlice({
   },
 });
 
-export const { toggleSidebar } = userSlice.actions;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
 export default userSlice.reducer;
