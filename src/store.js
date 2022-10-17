dae022ede0defcaf78c8dfd5a42b692cba3525db
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-17 21:31:00 +0300

    allJobs slice

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import jobSlice from "./features/user/job/jobSlice";
 import userSlice from "./features/user/userSlice";
+import allJobsSlice from "./features/user/allJobs/allJobsSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     job: jobSlice,
+    allJobs: allJobsSlice,
   },
 });
