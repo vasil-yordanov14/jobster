ea691f2148fbc3ec7ce00fafa321b9bcfc1fac3d
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-18 18:56:58 +0300

    toggle loading

--- src/features/user/allJobs/allJobsSlice.js
+++ src/features/user/allJobs/allJobsSlice.js
@@ -42,6 +42,14 @@ export const getAllJobs = createAsyncThunk(
 const allJobsSlice = createSlice({
   name: `allJobs`,
   initialState,
+  reducers: {
+    showLoading: (state) => {
+      state.isLoading = true;
+    },
+    hideLoading: (state) => {
+      state.isLoading = false;
+    },
+  },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
       state.isLoading = true;
@@ -57,4 +65,5 @@ const allJobsSlice = createSlice({
   },
 });
 
+export const { showLoading, hideLoading } = allJobsSlice.actions;
 export default allJobsSlice.reducer;
