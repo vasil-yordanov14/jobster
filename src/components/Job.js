150deef571fb536ef53e4157dfff28cc0cde25ca
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-18 18:50:41 +0300

    date format

--- src/components/Job.js
+++ src/components/Job.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Job";
 import { useDispatch } from "react-redux";
 import JobInfo from "./JobInfo";
+import moment from "moment/moment";
 
 const Job = ({
   _id,
@@ -16,7 +17,7 @@ const Job = ({
 }) => {
   const dispatch = useDispatch();
 
-  const date = createdAt;
+  const date = moment(createdAt).format(`MMM Do, YYYY`);
   return (
     <Wrapper>
       <header>
