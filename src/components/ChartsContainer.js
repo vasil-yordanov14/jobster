8bfaf5fd53d9c90ff304405799f488160da1b985
vasil-yordanov14 <vasil_sh@abv.bg>
2022-10-19 19:10:13 +0300

    Stats page structure

---
+++ src/components/ChartsContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsContainer = () => {
+  return <h2>ChartsContainer</h2>;
+};
+
+export default ChartsContainer;
--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,13 +1,23 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { StatsContainer, Loading, ChartsContainer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
 import { showStats } from "../../features/user/allJobs/allJobsSlice";
 
 const Stats = () => {
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(showStats());
   }, []);
-  return <h1>Stats</h1>;
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default Stats;
